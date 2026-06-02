234d3e51b9dad8c745c9c7c931d2e99786eafb83
omisan01 <omkar12mitake@gmail.com>
2026-06-02 18:13:22 +0530

    Feature: Get all menu items api

---
+++ app/api/menu/route.ts
@@ -0,0 +1,24 @@
+// COMPONENTS //
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+// UTILS //
+import { createClient } from "@/utils/supabase/server";
+
+export async function GET() {
+  const cookieStore = await cookies();
+  const supabase = await createClient(cookieStore);
+
+  const { data, error } = await supabase
+    .from("menu_items")
+    .select("*");
+
+  if (error) {
+    return NextResponse.json(
+      { error: error.message },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json(data);
+}
